2b416943293543a711fce076255c4808a32397d5
tsumagoi <164002237+tsumagoi@users.noreply.github.com>
2024-07-19 10:28:50 +0900

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const API_KEY = 'AIzaSyAP3rtRtZc0QL5h7nkb7HjTRPfmA_qeSlw';
-const SHEET_ID = '1chVxxOMC7X8qN1JvBZxXLprxFfPQm8_2-z1_7pld2o0';
+const SHEET_ID = '1iS7UeFlRGpKaw1d8r68Nu39Iv0UkLC0_rcYEwGC2plw';
